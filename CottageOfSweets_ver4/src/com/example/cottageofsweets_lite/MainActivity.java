527f5be008c922ecbcd9fb18226982824aceea7b
rujeerat <rujeerat.jit@gmail.com>
2015-01-06 21:50:30 +0700

    add comment in MainActivity

--- CottageOfSweets_ver4/src/com/example/cottageofsweets_lite/MainActivity.java
+++ CottageOfSweets_ver4/src/com/example/cottageofsweets_lite/MainActivity.java
@@ -31,7 +31,7 @@ public class MainActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		
 		//////////////////////////HEY///////////////////////////
-		/////////////////////////////////////////////////////////
+	
 
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
