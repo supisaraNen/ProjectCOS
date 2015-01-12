c97200b685bc765130dbdf0a84207475694c780a
rujeerat <rujeerat.jit@gmail.com>
2015-01-12 11:31:34 +0700

    add more comment

--- CottageOfSweets_ver4/src/com/example/cottageofsweets_lite/MainActivity.java
+++ CottageOfSweets_ver4/src/com/example/cottageofsweets_lite/MainActivity.java
@@ -30,6 +30,11 @@ public class MainActivity extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 				
+		///////////////////////////////////////////
+		////
+		///
+		///
+	
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
